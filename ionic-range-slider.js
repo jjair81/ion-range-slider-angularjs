6e6d83a93e719e2353bfc338047f9784deb7a757
jjair81 <jayjair@hotmail.com>
2016-08-19 11:43:33 -0700

    Added checks to see if From and To were set in the first place to reduce error logging

--- ionic-range-slider.js
+++ ionic-range-slider.js
@@ -5,94 +5,98 @@
 angular.module("ion.rangeslider", []);
 
 angular.module("ion.rangeslider").directive("ionRangeSlider", [
-    function () {
-        
-        return {
-            restrict: "E",
-            scope: {
-                min: "=",
-                max: "=",
-                type: "@",
-                prefix: "@",
-                maxPostfix: "@",
-                prettify: "@",
-                grid: "@",
-                gridMargin: "@",
-                postfix: "@",
-                step: "@",
-                hideMinMax: "@",
-                hideFromTo: "@",
-                from: "=",
-                to: "=",
-                disable: "=",
-                onChange: "&",
-                onFinish: "&"
-            },
-            replace: true,
-            link: function ($scope, $element) {
-                $element.ionRangeSlider({
-                    min: $scope.min,
-                    max: $scope.max,
-                    type: $scope.type,
-                    prefix: $scope.prefix,
-                    maxPostfix: $scope.maxPostfix,
-                    prettify: $scope.prettify,
-                    grid: $scope.grid,
-                    gridMargin: $scope.gridMargin,
-                    postfix: $scope.postfix,
-                    step: $scope.step,
-                    hideMinMax: $scope.hideMinMax,
-                    hideFromTo: $scope.hideFromTo,
-                    from: $scope.from,
-                    to: $scope.to,
-                    disable: $scope.disable,
-                    onChange: function (a) {
-                        $scope.$apply(function () {
-                            $scope.from = a.from;
-                            $scope.to = a.to;
-                            $scope.onChange && $scope.onChange({
-                                a: a
-                            });
-                        });
-                    },
-                    onFinish: function () {
-                        $scope.$apply($scope.onFinish);
-                    }
-                });
-                var watchers = [];
-                watchers.push($scope.$watch("min", function (value) {
-                    $element.data("ionRangeSlider").update({
-                        min: value
-                    });
-                }));
-                watchers.push($scope.$watch('max', function (value) {
-                    $element.data("ionRangeSlider").update({
-                        max: value
-                    });
-                }));
-                watchers.push($scope.$watch('from', function (value) {
-                    var slider = $element.data("ionRangeSlider");
-                    if (slider.old_from !== value) {
-                        slider.update({
-                            from: value
-                        });
-                    }
-                }));
-                watchers.push($scope.$watch('to', function (value) {
-                    var slider = $element.data("ionRangeSlider");
-                    if (slider.old_to !== value) {
-                        slider.update({
-                            to: value
-                        });
-                    }
-                }));
-                watchers.push($scope.$watch('disable', function (value) {
-                    $element.data("ionRangeSlider").update({
-                        disable: value
-                    });
-                }));
-            }
-        }
-        
+  function() {
+
+    return {
+      restrict: "E",
+      scope: {
+        min: "=",
+        max: "=",
+        type: "@",
+        prefix: "@",
+        maxPostfix: "@",
+        prettify: "@",
+        grid: "@",
+        gridMargin: "@",
+        postfix: "@",
+        step: "@",
+        hideMinMax: "@",
+        hideFromTo: "@",
+        from: "=",
+        to: "=",
+        disable: "=",
+        onChange: "&",
+        onFinish: "&"
+      },
+      replace: true,
+      link: function($scope, $element) {
+        $element.ionRangeSlider({
+          min: $scope.min,
+          max: $scope.max,
+          type: $scope.type,
+          prefix: $scope.prefix,
+          maxPostfix: $scope.maxPostfix,
+          prettify: $scope.prettify,
+          grid: $scope.grid,
+          gridMargin: $scope.gridMargin,
+          postfix: $scope.postfix,
+          step: $scope.step,
+          hideMinMax: $scope.hideMinMax,
+          hideFromTo: $scope.hideFromTo,
+          from: $scope.from,
+          to: $scope.to,
+          disable: $scope.disable,
+          onChange: function(a) {
+            $scope.$apply(function() {
+              if ($scope.from !== undefined) {
+                $scope.from = a.from;
+              }
+              if ($scope.to !== undefined) {
+                $scope.to = a.to;
+              }
+              $scope.onChange && $scope.onChange({
+                a: a
+              });
+            });
+          },
+          onFinish: function() {
+            $scope.$apply($scope.onFinish);
+          }
+        });
+        var watchers = [];
+        watchers.push($scope.$watch("min", function(value) {
+          $element.data("ionRangeSlider").update({
+            min: value
+          });
+        }));
+        watchers.push($scope.$watch('max', function(value) {
+          $element.data("ionRangeSlider").update({
+            max: value
+          });
+        }));
+        watchers.push($scope.$watch('from', function(value) {
+          var slider = $element.data("ionRangeSlider");
+          if (slider.old_from !== value) {
+            slider.update({
+              from: value
+            });
+          }
+        }));
+        watchers.push($scope.$watch('to', function(value) {
+          var slider = $element.data("ionRangeSlider");
+          if (slider.old_to !== value) {
+            slider.update({
+              to: value
+            });
+          }
+        }));
+        watchers.push($scope.$watch('disable', function(value) {
+          $element.data("ionRangeSlider").update({
+            disable: value
+          });
+        }));
+      }
     }
+
+  }
 ])
